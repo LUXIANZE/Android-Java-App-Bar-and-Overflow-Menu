d8d6a1d05930b0a38d7a12b662f51994da7291e7
LUXIANZE <luxianze98@gmail.com>
2020-12-16 13:34:48 +0800

    Init Menu setup

--- app/src/main/java/com/example/androidjavaappbarandoverflowmenu/MainActivity.java
+++ app/src/main/java/com/example/androidjavaappbarandoverflowmenu/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.androidjavaappbarandoverflowmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.option2:
+                toastMessageService(item.getTitle());
+                return true;
+            case R.id.option1:
+                toastMessageService(item.getTitle());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void toastMessageService(CharSequence message){
+        Context context = getApplicationContext();
+        CharSequence text = message + " Selected";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
