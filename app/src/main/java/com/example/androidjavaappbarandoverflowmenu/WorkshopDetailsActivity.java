807516712f4123586f22f4cddd9ca8614a8f72d2
LUXIANZE <luxianze98@gmail.com>
2020-12-18 07:48:04 +0800

    Add new activities - ProfileActivity - WorkshopDetailsActivity

---
+++ app/src/main/java/com/example/androidjavaappbarandoverflowmenu/WorkshopDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidjavaappbarandoverflowmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkshopDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workshop_details);
+    }
+}
