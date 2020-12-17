76d3404f36be304c4e87f3e52c9faf97a9c8e846
eunicechy <eunicechy@siswa.um.edu.my>
2020-12-17 22:39:10 +0800

    added nav bar activity and chat icon

---
+++ app/src/main/java/com/example/androidjavaappbarandoverflowmenu/NavigationActivity.java
@@ -0,0 +1,45 @@
+package com.example.androidjavaappbarandoverflowmenu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        Toolbar myChildToolBar = (Toolbar)findViewById(R.id.toolbarNav);
+        setSupportActionBar(myChildToolBar);
+
+        ActionBar ab = getSupportActionBar();
+
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.option1:
+                // User chose the "settings" items, show the app settings UI...
+                return true;
+            case R.id.action_chat:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
